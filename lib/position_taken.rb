8812fc6590405eef48233c683cb33d96e7cb5e84
tal shachar <tal.shachar@buzzfeed.com>
2017-12-04 06:58:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-board[index] == ( "" || " " || "nil")
-board[index] == ("X" || "O")
+board[index] != ( "" || " " || "nil")
+board[index] != ("X" || "O")
 
 end
